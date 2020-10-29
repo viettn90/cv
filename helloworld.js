0ec722db68ada456ce20c5a1759c1e8620d510c0
Viet.Tran <viettn90@gmail.com>
2020-10-29 09:25:37 +0700

    some change

---
+++ helloworld.js
@@ -0,0 +1,4 @@
+var years = [2016, 2017, 2018, 2019];
+for (var i = 0; i <= years.length; i++) {
+    console.log(years[i]);
+}
---
+++ helloworld.ts
@@ -0,0 +1,4 @@
+let years: Array<number> = [2016, 2017, 2018,2019];
+for (let i=0; i<=years.length; i++) {
+    console.log(years[i]);
+}
